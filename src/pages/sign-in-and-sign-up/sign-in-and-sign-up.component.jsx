7bb3eb2fbccd44f60b0034728e1ef663badd17f3
bikal1000 <bikal1000.bs@gmail.com>
2020-03-27 09:44:32 +0545

    updating our app to redirect to home if user is signed in

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -5,11 +5,11 @@ import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
 
-const signInAndSignUpPage = () => (
+const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
     <SignUp />
   </div>
 );
 
-export default signInAndSignUpPage;
+export default SignInAndSignUpPage;
